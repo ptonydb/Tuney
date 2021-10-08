a89b96e8f1c9a47e89b56520f2b20125bc076b80
Tony Duong <phatduong@cpp.edu>
2021-10-08 01:03:49 -0700

    Removed personal BOT token

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 import music
+from config.config import *
 
 cogs = [music]
 
@@ -9,4 +10,4 @@ bot = commands.Bot(command_prefix='!', intents = discord.Intents.all())
 for i in range(len(cogs)):
     cogs[i].setup(bot)
 
-bot.run("ODk1NTEzMjI5MDIxODM5MzYw.YV5prw.2KXyXq5J1U-Gg2nJHnBMfcKN6ec")
+bot.run(token)
